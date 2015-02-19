522f0a487ae62892cac77f5c794018eda065cdab
Chung Eun Kim <kce0423@gmail.com>
2015-02-19 15:20:42 -0800

    Revised CONFIG.py and created common.py for common functions

--- CONFIG.py
+++ CONFIG.py
@@ -1,11 +1,11 @@
-import numpy as np
-import csv
-
 # Number of samples in the dataset
 NUM_TRAININGS = 4000
 
 # NUmber of samples in the test dataset
 NUM_TEST = 9868
+
+# Types of data
+types = ['raw', 'tf_idf']
 
 # Filenames of training data
 fin_names = {'raw':'data/kaggle_train_wc.csv', 'tf_idf':'data/kaggle_train_tf_idf.csv'}
@@ -16,6 +16,9 @@ ftest_names = {'raw':'data/kaggle_test_wc.csv', 'tf_idf':'data/kaggle_test_tf_id
 # Cross Validation 
 K = 5
 
+# Type of scoring functions
+scores = ['accuracy', 'average_precision', 'precision', 'recall', 'f1']
+
 # Decision Tree parameters
 criterions = ["gini", "entropy"]
 splitters = ["best", "random"]
@@ -25,45 +28,20 @@ min_samples_splits = range(2, 500)
 max_leaf_nodes = [None] + range(1, 100)
 
 # SVM parameters
-Cs = [1, 0.5, 0.1, 0.05, 0.01, 0.005, 0.001, 0.0005, 0.0001]
-kernels = ['rbf', 'linear', 'poly', 'sigmoid']
-degrees = range(1,5)
-# gamma
-# ceof0
-# tol
-# max_iter
+param_grid_SVM = [
+  {'C': [1000, 100, 10, 1, 0.1, 0.01, 0.001, 0.0001],
+   'kernel': ['linear']},
+  {'C': [1000, 100, 10, 1, 0.1, 0.01, 0.001, 0.0001],
+   'kernel': ['poly'],
+   'degree': [1, 2, 3, 4, 5],
+   'gamma': [0.0, 1, 0.1, 0.01, 0.001, 0.0001],
+   'coef0': [0.0, 1, 10, 100]},
+  {'C': [1000, 100, 10, 1, 0.1, 0.01, 0.001, 0.0001],
+   'kernel': ['rbf'],
+   'gamma': [0.0, 1, 0.1, 0.01, 0.001, 0.0001]},
+  {'C': [1000, 100, 10, 1, 0.1, 0.01, 0.001, 0.0001],
+   'kernel': ['sigmoid'],
+   'gamma': [0.0, 1, 0.1, 0.01, 0.001, 0.0001],
+   'coef0': [0.0, 1, 10, 100]}
+]
 
-# Return error rate
-def get_error(G, Y):
-    error = 0
-    for i in range(len(G)):
-        if G[i] != Y[i]:
-            error += 1
-    return 1.0 * error / len(G)
-
-# Return X_train and Y_train
-def getXY(typeOfData):
-    with open(fin_names[typeOfData], 'r') as fin:
-        data = np.array(list(csv.reader(fin)))
-
-    X = data[1:NUM_TRAININGS+1, 1:-1]
-    Y = data[1:NUM_TRAININGS+1, -1]
-    return X, Y
-
-# Output submission file
-def output_submission(typeOfData, clf, filename):
-    # Retrieve train data
-    X,Y = getXY(typeOfData)
-
-    # Predict with the maximum model
-    clf = clf.fit(X, Y)
-    with open(ftest_names[typeOfData], 'r') as ftest:
-        data = np.array(list(csv.reader(ftest)))
-
-    # Write to submission file
-    X_test = data[1:NUM_TEST+1, 1:]
-    G = clf.predict(X_test)
-    with open(filename, 'w') as predict:
-        predict.write("Id,Prediction")
-        for i in range(NUM_TEST):
-            predict.write("\n{},{}", i, G[i])
---
+++ common.py
@@ -0,0 +1,53 @@
+import numpy as np
+import csv
+from CONFIG import *
+
+# Return error rate
+def get_error(G, Y):
+    error = 0
+    true_pos = 0
+    true_neg = 0
+    false_pos = 0
+    false_neg = 0
+    for i in range(len(G)):
+        if G[i] != Y[i]:
+            error += 1
+        if G[i] == 1 and Y[i] == 1:
+            true_pos += 1
+        elif G[i] == 0 and Y[i] == 0:
+            true_neg += 1
+        elif G[i] == 0 and Y[i] == 1:
+            false_pos += 1
+        else:
+            false_neg += 1
+
+    error = 1.0 * error / len(G)
+    precision = 1.0 * true_pos / (true_pos + false_pos)
+    recall = 1.0 * true_pos / (true_pos + false_neg)
+    return  error, precision, recall
+
+# Return X_train and Y_train
+def getTrainData(typeOfData):
+    with open(fin_names[typeOfData], 'r') as fin:
+        data = np.array(list(csv.reader(fin)))
+
+    X = data[1:NUM_TRAININGS+1, 1:-1]
+    Y = data[1:NUM_TRAININGS+1, -1]
+    return X, Y
+
+
+# Return X_test
+def getTestData(typeOfData):
+    with open(ftest_names[typeOfData], 'r') as ftest:
+        data = np.array(list(csv.reader(ftest)))
+    return data[1:NUM_TEST+1, 1:]
+
+
+# Output submission file
+def output_submission(typeOfData, clf, filename):
+    X_test = getTestData(typeOfData)
+    G = clf.predict(X_test)
+    with open(filename, 'w') as predict_f:
+        predict_f.write("Id,Prediction")
+        for i in range(NUM_TEST):
+            predict_f.write("\n{},{}", i, G[i])
