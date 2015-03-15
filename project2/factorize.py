b843416f4aa40b9d251034470a3122dbb2fd4b63
Chung Eun Kim <kce0423@gmail.com>
2015-03-15 00:28:56 -0700

    Factorize and visualizer updated / Output files removed

--- project2/factorize.py
+++ project2/factorize.py
@@ -6,35 +6,33 @@ NUM_USERS = 943
 NUM_MOVIES = 1682
 K = 20
 
-def load_data(filename):
-    data_file = filename.replace(".txt", ".sframe")
+DATA_FILE = 'data/data.txt'
+DATA_SFRAME = 'data/data.sframe'
+
+def load_data():
     data = None
-    if os.path.exists(data_file):
+    if os.path.exists(DATA_SFRAME):
         print "Loading data..."
         data = gl.load_sframe(data_file)
     else:
         print "Creating data..."
         raw_data = {'user_id': [], 'item_id': [], 'rating': []}
-        with open(filename, 'r') as f:
+        with open(DATA_FILE, 'r') as f:
             lines = f.readlines()[0].split('\r')
             for line in lines:
                 raw_data['user_id'].append(line.split('\t')[0])
                 raw_data['item_id'].append(line.split('\t')[1])
                 raw_data['rating'].append(int(line.split('\t')[2]))
         data = gl.SFrame(raw_data)
-        data.save(data_file)
+        data.save(DATA_SFRAME)
     return data
  
-def factorize(data):
+def factorize(data, filename):
     print "Factorizing..."
     m = gl.recommender.factorization_recommender.create(
             data, target='rating', num_factors=K, 
             #regularization=10, linear_regularization=10,
             max_iterations=100000, solver='sgd')
-
-    # Print the summary
-    m.summary()
-    print m.get("coefficients")
 
     # Save the model
     modelname = filename.replace(".txt", ".model")
@@ -47,7 +45,6 @@ def export(m, filename):
     m.summary()
     print m['coefficients']
     print "Sorting..."
-    matrixname = filename.replace(".txt", ".matrix")
     U_lst = [None] * NUM_USERS
     V_lst = [None] * NUM_MOVIES
     U_idx = list(m['coefficients']['user_id']['user_id'])
@@ -58,7 +55,7 @@ def export(m, filename):
         V_lst[int(movie)-1] = map(str, m['coefficients']['item_id']['factors'][i])
 
     print "Writing..."
-    with open(matrixname, 'w') as f:
+    with open(filename, 'w') as f:
         for row in U_lst:
             f.write('\t'.join(row))
             f.write('\n')
@@ -67,14 +64,19 @@ def export(m, filename):
             f.write('\n')
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print "Usage: python factorize.py [data_filename]"
-        print "e.g. python collab_filter.py data.txt"
+    if len(sys.argv) < 3:
+        print "Usage: python factorize.py [data] [output] [optional:model]"
+        print "e.g. python collab_filter.py data.txt out.txt model.txt"
         sys.exit(1)
 
-    filename = sys.argv[1]
-    data = load_data(filename)
-    m = factorize(data)
-    #m = gl.load_model(filename.replace(".txt",".model"))
-    export(m, filename)
+    in_file = sys.argv[1]
+    out_file = sys.argv[2]
+    if len(sys.argv) > 3:
+        model_file = sys.argv[3]
+        m = gl.load_model(model_file)
+    else:
+        data = load_data(in_file)
+        m = factorize(data, out_file)
+
+    export(m, out_file)
 
--- project2/visualize.py
+++ project2/visualize.py
@@ -1,4 +1,3 @@
-import graphlab as gl
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
@@ -6,6 +5,8 @@ import sys
 NUM_USERS = 943
 NUM_MOVIES = 1682
 K = 20
+
+MOVIE_FILE = 'data/movies.txt'
 
 INTERESTING = ['My Fair Lady', 'Clockwork Orange', 'Free Willy 2: The Adventure Home', 'Free Willy', 'Batman Forever', 'Bad Boys',
         'Birdcage', 'Nutty Professor', 'GoldenEye', 'Apollo 13', 'Twelve Monkeys', '2001: A Space Odyssey',
@@ -29,11 +30,11 @@ def parse(matrix):
 
     return U, V
 
-def get_movies(movie):
+def get_movies():
     print "Extract movie info..."
     movies = {}
     movies_idx = [''] * NUM_MOVIES
-    with open(movie, 'r') as f:
+    with open(MOVIE_FILE, 'r') as f:
         lines = f.readlines()[0].split('\r')
         for line in lines:
             idx = int(line.split('\t')[0])-1
@@ -55,19 +56,20 @@ def top_rated():
     return np.argsort(movie_cnt)
 
 
+
 def visualize(U, V, movies, movies_idx):
     print "Compute SVD of V..."
     V = V - np.mean(V, axis=1)[:, None]
     print 'Mean = ', np.mean(V, axis=1)
     A, s, B = np.linalg.svd(V)
     print A.shape, s.shape, B.shape
-    V_tilde = np.dot(A[:,:2].T, V)
+    V_tilde = np.dot(A[:,np.array([0,1])].T, V)
     V_tilde = V_tilde / np.std(V_tilde, axis=1)[:, None]
     print 'Variance = ', np.var(V_tilde, axis=1)
 
     #interest = np.array([movies[name]['idx'] for name in INTERESTING])
     interest = top_rated()[-40:]
-    print interest
+    #interest = np.arange(40)
 
     movie_x = V_tilde[0, interest]
     movie_y = V_tilde[1, interest]
@@ -79,13 +81,12 @@ def visualize(U, V, movies, movies_idx):
 
  
 if __name__ == "__main__":
-    if len(sys.argv) < 3:
-        print "Usage: python visualize.py [matrix_filename] [movie_filename]"
-        print "e.g. python collab_filter.py data.matrix movie.txt"
+    if len(sys.argv) < 2:
+        print "Usage: python visualize.py [matrix_filename]"
+        print "e.g. python collab_filter.py matrix.txt"
         sys.exit(1)
 
     matrix = sys.argv[1]
-    movie = sys.argv[2]
     U, V = parse(matrix)
-    movies, movies_idx = get_movies(movie)
+    movies, movies_idx = get_movies()
     visualize(U, V, movies, movies_idx)
